9cf4cc68b647cc4e8f58f4693ec2a252739e199e
tenthyoung <tenthyoung@gmail.com>
2021-03-07 10:33:12 -0800

    feat(site): finished logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <script>
   import { LottiePlayer } from "@lottiefiles/svelte-lottie-player";
   let isUserBroke = false;
+  let userCanAfford = "unknown";
   let brokeClickCount = 0;
+  let thoughtCount = -1;
+  let showMoneyButton = true;
+
   const greeting =
     "Alright, pay me a penny for a thought. Buy now while the sale lasts.";
   const brokeDialogue = [
@@ -13,25 +17,10 @@
     "Is this how you haggle..? You just keep telling us you can't afford it. Ain't gonna work, mate.",
   ];
   const thoughts = [
-    "I'm a little concerned about my buff friend in the back, he has a 12-pack. It seems a bit vain.",
+    "What am I thinking..? I'm a little concerned about my buff friend in the back, he has a 12-pack. It seems a bit vain.",
     "When I roll out with the boys, you might have to hold the honeys back",
-    "Who knew you could sell a thought?",
-    "Stanley in the back brought his dinner to work... The nerve, but we gotta keep him on board since his dad funds us.",
-  ];
-
-  let controlsLayout = [
-    "previousFrame",
-    "playpause",
-    "stop",
-    "nextFrame",
-    "progress",
-    "frame",
-    "loop",
-    "spacer",
-    "background",
-    "snapshot",
-    "zoom",
-    "info",
+    "Stanley in the back brought his lunch to work... The nerve, but we gotta keep him on board since his dad funds us.",
+    "My final thought... thanks for being a valuable customer. I'm all out of thoughts.",
   ];
 
   function handlePoorPerson() {
@@ -42,6 +31,17 @@
       brokeClickCount = 0;
     }
   }
+
+  function userPaidAPenny(payment) {
+    thoughtCount++;
+    userCanAfford = true;
+
+    if (thoughtCount === thoughts.length - 1) {
+      showMoneyButton = false;
+    }
+  }
+
+  $: console.log(thoughtCount);
 </script>
 
 <main>
@@ -53,41 +53,47 @@
       loop={true}
       renderer="svg"
       background="transparent"
-      height={300}
-      {controlsLayout}
     />
   </div>
   <div class="dialogue">
     {#if isUserBroke}
       <p>{brokeDialogue[brokeClickCount]}</p>
+    {:else if userCanAfford === true}
+      <p>{thoughts[thoughtCount]}</p>
     {:else}
       <p>{greeting}</p>
     {/if}
   </div>
   <div id="user-controls">
-    <div
-      class="money-button"
-      data-to="tenthyoung@moneybutton.com"
-      data-amount="0.01"
-      data-label="For a thought"
-      data-currency="USD"
-    />
-    <button class="secondary" on:click={handlePoorPerson}
-      >I can't afford that</button
-    >
+    {#if showMoneyButton}
+      <div
+        class="money-button"
+        data-to="tenthyoung@moneybutton.com"
+        data-amount="0.01"
+        data-label="For a thought"
+        data-currency="USD"
+        data-dev-mode="true"
+        data-on-payment="userPaidAPenny"
+      />
+    {/if}
+    {#if userCanAfford === "unknown"}
+      <button class="secondary" on:click={handlePoorPerson}
+        >I can't afford that</button
+      >
+    {/if}
   </div>
 </main>
 
 <style>
   main {
-    text-align: center;
-    padding: 1rem;
     margin: 0 auto;
+    text-align: center;
+    width: 100%;
   }
 
   h1 {
     color: #ff3e00;
-    font-size: 4em;
+    font-size: 3rem;
     font-weight: 100;
   }
 
@@ -112,11 +118,6 @@
     width: 11rem !important;
   }
 
-  #lottie-wrap {
-    height: 500px;
-    overflow: hidden;
-  }
-
   #user-controls {
     display: flex;
     flex-direction: column;
@@ -126,11 +127,5 @@
 
   #user-controls button {
     margin-top: 1rem;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
   }
 </style>
